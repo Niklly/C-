9fb353b7ac48d7949f4356343bc76dd97a2ca446
Nikelly Thomaz <113486704+Niklly@users.noreply.github.com>
2024-07-21 20:55:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int n = 0;
+Console.Write("Escreva uma idade: ");
+int.TryParse(Console.ReadLine(), out n);
+Console.ReadKey();
+
+if (n > 14)
+{
+    if (n > 18)
+    {
+        Console.WriteLine("Adulto");
+    }
+
+    else
+    {
+        Console.WriteLine("Adolescente");
+    }
+}
+
+else
+{
+    if (n > 0)
+    {
+        Console.WriteLine("Criança");
+    }
+    else
+    {
+        Console.WriteLine("Algo está errado");
+    }
+}
+Console.ReadKey();
